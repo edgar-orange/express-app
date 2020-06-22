9cdff25577cd022b22d8bb9894af586d15dead0a
Miguel <miguel-orange@users.noreply.github.com>
2020-06-22 16:16:36 -0700

    connected api to mock data, practicing async/await and destructuring

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-
 const express = require('express')
+const superagent = require("superagent");
 const app = express()
 const port = 3000
+const x = {
+    book: "test",
+    year: 129
+};
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', async ({ query: { id } }, res) => {
+    try {
+        const { body: { name, gender, height } } = await superagent.get("https://swapi.dev/api/people/1");
 
+        res.json({ name, gender, height })
+    } catch(err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+})
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
