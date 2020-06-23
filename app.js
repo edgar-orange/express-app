17ae7b7af610c73285db9e0558a6d255e176d991
edgar-orange <EDGAR_AGUIRRE@homedepot.com>
2020-06-23 12:11:32 -0500

    added chalk library

--- app.js
+++ app.js
@@ -2,7 +2,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const chalk = require('chalk')
+
+// const debug = require('debug')('app')
+//change console.log to debug 
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
